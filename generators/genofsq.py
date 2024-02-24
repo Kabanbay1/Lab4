f00cdff6c08f6dd03f6a1d8e4fc6016ca2066b8b
Kabanbay1 <156914140+Kabanbay1@users.noreply.github.com>
2024-02-24 11:48:55 +0600

    Delete generators directory

--- generators/genofsq.py
+++
@@ -1,9 +0,0 @@
-def Sqrgen(N): 
-    a=1
-    while a!=N:
-        yield a*a
-        a+=1
-N=int(input("Limit of Squaring: "))
-x=Sqrgen(N)
-for i in range (N-1):
-    print(next(x))
--- generators/genofsqares.py
+++
@@ -1,8 +0,0 @@
-def sqares(a,b):
-    if a!=b+1:
-        yield a*a
-a=int(input("1: "))
-b=int(input("2: "))
-for i in range((b+1)-a):
-    x=sqares(a+i,b)
-    print(next(x))
